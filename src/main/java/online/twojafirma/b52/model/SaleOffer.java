2428b3cb4a013a02bce4a8c68d86f4234fd93a67
Piotr <piotrklopotowski@outlook.com>
2023-10-07 00:16:06 +0200

    Add models for offers

---
+++ src/main/java/online/twojafirma/b52/model/SaleOffer.java
@@ -0,0 +1,176 @@
+package online.twojafirma.b52.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "sale_offers")
+public class SaleOffer {
+    @Id
+    @Column(insertable=false, updatable=false)
+    private int id;
+    @Column(insertable=false, updatable=false)
+    private String url;
+
+    @Column(insertable=false, updatable=false)
+    private String offerName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "district", insertable=false, updatable=false)
+    private District district;
+
+    @Column(insertable=false, updatable=false)
+    private String market;
+
+    @Column(insertable=false, updatable=false)
+    private LocalDate addedDate;
+
+    @Column(insertable=false, updatable=false)
+    private String entryDateYearMonth;
+
+    @Column(insertable=false, updatable=false)
+    private LocalDateTime dateScrapped;
+
+    @Column(insertable=false, updatable=false)
+    private LocalDateTime dateLastUpdated;
+
+    @Column(insertable=false, updatable=false)
+    private String websiteScrappedFrom;
+
+    @Column(insertable=false, updatable=false)
+    private float firstPrice;
+
+    @Column(insertable=false, updatable=false)
+    private float latestPrice;
+
+    @Column(insertable=false, updatable=false)
+    private float sizeSquareMeters;
+
+    @Column(insertable=false, updatable=false)
+    private float firstPricePerSquareMeter;
+
+    @Column(insertable=false, updatable=false)
+    private float latestPricePerSquareMeter;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getOfferName() {
+        return offerName;
+    }
+
+    public void setOfferName(String offerName) {
+        this.offerName = offerName;
+    }
+
+    public District getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(District district) {
+        this.district = district;
+    }
+
+    public String getMarket() {
+        return market;
+    }
+
+    public void setMarket(String market) {
+        this.market = market;
+    }
+
+    public LocalDate getAddedDate() {
+        return addedDate;
+    }
+
+    public void setAddedDate(LocalDate addedDate) {
+        this.addedDate = addedDate;
+    }
+
+    public String getEntryDateYearMonth() {
+        return entryDateYearMonth;
+    }
+
+    public void setEntryDateYearMonth(String entryDateYearMonth) {
+        this.entryDateYearMonth = entryDateYearMonth;
+    }
+
+    public LocalDateTime getDateScrapped() {
+        return dateScrapped;
+    }
+
+    public void setDateScrapped(LocalDateTime dateScrapped) {
+        this.dateScrapped = dateScrapped;
+    }
+
+    public LocalDateTime getDateLastUpdated() {
+        return dateLastUpdated;
+    }
+
+    public void setDateLastUpdated(LocalDateTime dateLastUpdated) {
+        this.dateLastUpdated = dateLastUpdated;
+    }
+
+    public String getWebsiteScrappedFrom() {
+        return websiteScrappedFrom;
+    }
+
+    public void setWebsiteScrappedFrom(String websiteScrappedFrom) {
+        this.websiteScrappedFrom = websiteScrappedFrom;
+    }
+
+    public float getFirstPrice() {
+        return firstPrice;
+    }
+
+    public void setFirstPrice(float firstPrice) {
+        this.firstPrice = firstPrice;
+    }
+
+    public float getLatestPrice() {
+        return latestPrice;
+    }
+
+    public void setLatestPrice(float latestPrice) {
+        this.latestPrice = latestPrice;
+    }
+
+    public float getSizeSquareMeters() {
+        return sizeSquareMeters;
+    }
+
+    public void setSizeSquareMeters(float sizeSquareMeters) {
+        this.sizeSquareMeters = sizeSquareMeters;
+    }
+
+    public float getFirstPricePerSquareMeter() {
+        return firstPricePerSquareMeter;
+    }
+
+    public void setFirstPricePerSquareMeter(float firstPricePerSquareMeter) {
+        this.firstPricePerSquareMeter = firstPricePerSquareMeter;
+    }
+
+    public float getLatestPricePerSquareMeter() {
+        return latestPricePerSquareMeter;
+    }
+
+    public void setLatestPricePerSquareMeter(float latestPricePerSquareMeter) {
+        this.latestPricePerSquareMeter = latestPricePerSquareMeter;
+    }
+}
