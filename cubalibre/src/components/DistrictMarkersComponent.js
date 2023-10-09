479ce6855f2bbd573a47befc6522e377d55fb3eb
Piotr <piotrklopotowski@outlook.com>
2023-10-09 12:52:44 +0200

    Add markers

--- cubalibre/src/components/DistrictMarkersComponent.js
+++ cubalibre/src/components/DistrictMarkersComponent.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react"
 import {Marker} from "react-leaflet";
 
+
+const onDistrictMarkerClicked = (districtId, districtName, districtCity) => {
+  alert("marker clicked " + districtId + ", " + districtName + ", " + districtCity)
+}
+
 const DistrictsMarkersComponent = () => {
   const [districts, setDistricts] = useState([])
 
@@ -27,6 +32,11 @@ const DistrictsMarkersComponent = () => {
               district.lat,
               district.lon
             ]}
+            eventHandlers={{
+              click: (e) => {
+                onDistrictMarkerClicked(district.id, district.name, district.city)
+              },
+            }}
         />
       ))}
     </>
