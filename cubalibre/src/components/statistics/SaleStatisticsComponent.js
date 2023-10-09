6e220372f43d28010a04dcb01abeff0f2cc4b490
Piotr <piotrklopotowski@outlook.com>
2023-10-10 00:41:04 +0200

    Add switch to change chart value

--- cubalibre/src/components/statistics/SaleStatisticsComponent.js
+++ cubalibre/src/components/statistics/SaleStatisticsComponent.js
@@ -10,7 +10,35 @@ import { pl } from 'date-fns/locale';
 const SaleStatisticsComponent = ({saleStatistics}) => {
     Chart.register(CategoryScale, LinearScale);
 
-    const [chartData, setChartData] = useState();
+    const [chartData, setChartData] = useState({
+        market: "primary",
+        key: "latestPrice",
+        title: "Average price [PLN]"
+    });
+
+    const onPriceClicked = () => {
+        setChartData({
+            market: chartData["market"],
+            key: "latestPrice",
+            title: "Average price [PLN]"
+        });
+    };
+
+    const onPricePerSquareMeterClicked = () => {
+        setChartData({
+            market: chartData["market"],
+            key: "latestPricePerSquareMeter",
+            title: "Average price per m^2 [PLN/m^2]"
+        });
+    };
+
+    const onSizeClicked = () => {
+        setChartData({
+            market: chartData["market"],
+            key: "sizeSquareMeters",
+            title: "Average size in m^2"
+        });
+    };
 
 
 
@@ -19,17 +47,20 @@ const SaleStatisticsComponent = ({saleStatistics}) => {
     }
 
     return (
-        <div className="sidebar-component__sale-statistics">
-            <div className="sidebar-component__sale-statistics__title">
-                Sale statistics
-            </div>
+        <div className="sidebar-component__statistics">
+
+            <ul className="tabs sidebar-component__statistics__tabs">
+                <li className="tab col s3"><a href="#" onClick={onPriceClicked}>Price</a></li>
+                <li className="tab col s3"><a href="#" onClick={onPricePerSquareMeterClicked}>Price/m<sup>2</sup></a></li>
+                <li className="tab col s3"><a href="#" onClick={onSizeClicked}>Size</a></li>
+            </ul>
 
             <Line
                 data={{
                     labels: saleStatistics.map(entry => new Date(entry['entryDateYearMonth'] + "-01")),
                     datasets: [{
                         label: 'Sale prices',
-                        data: saleStatistics.map(entry => entry['latestPrice']),
+                        data: saleStatistics.map(entry => entry[chartData['key']]),
                         borderColor: "#ee6e73",
                         fill: false,
                         cubicInterpolationMode: 'monotone',
@@ -41,7 +72,7 @@ const SaleStatisticsComponent = ({saleStatistics}) => {
                     plugins: {
                         title: {
                             display: true,
-                            text: "Title"
+                            text: chartData['title']
                         },
                         legend: {
                             display: false
